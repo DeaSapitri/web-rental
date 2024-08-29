63f71de2224af739790283ece80bd6eaaeaa9352
sailahandayani <saylahandayani90@gmail.com>
2024-08-29 09:25:46 +0800

    tambah codingan edit hapus

--- edit_transaksi.php
+++ edit_transaksi.php
@@ -15,22 +15,28 @@
 		<form method="post" action="edit_transaksi.php">
 			<table>
 				<tr>			
-					<td>Nama</td>
+					<td>id_transaksi</td>
 					<td>
 						<input type="hidden" name="id_transaksi" value="<?php echo $d['id_transaksi']; ?>">
+                    </td>
+                    </tr>
+
+                <tr>			
+					<td>id_mobil</td>
+					<td>
 						<input type="number" name="id_mobil" value="<?php echo $d['id_mobil']; ?>">
 					</td>
 				</tr>
 				<tr>
-					<td>NIM</td>
+					<td>id_costumer</td>
 					<td><input type="number" name="id_costumer" value="<?php echo $d['id_costumer']; ?>"></td>
 				</tr>
 				<tr>
-					<td>Alamat</td>
+					<td>tgl_sewa</td>
 					<td><input type="text" name="tgl_sewa" value="<?php echo $d['tgl_sewa']; ?>"></td>
 				</tr>
                 <tr>
-					<td>Alamat</td>
+					<td>tgl_kembali</td>
 					<td><input type="text" name="tgl_kembali" value="<?php echo $d['tgl_kembali']; ?>"></td>
 				<tr>
 					<td></td>
